8bf1cc71ebecce9705fe55d844eb76dfd4a38c5c
Alex <133868982+AlexBozzo@users.noreply.github.com>
2023-05-29 20:07:30 +0200

    Delete Spring Projects/passport/src/main/java/com/alex/passport directory

--- Spring Projects/passport/src/main/java/com/alex/passport/repository/CittadinoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alex.passport.repository;
-
-import com.alex.passport.entity.Citizien;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CittadinoRepository extends JpaRepository<Citizien, Integer> {
-}
--- Spring Projects/passport/src/main/java/com/alex/passport/repository/RichiestaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alex.passport.repository;
-
-import com.alex.passport.entity.OperationRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RichiestaRepository extends JpaRepository<OperationRequest, Integer> {
-}
